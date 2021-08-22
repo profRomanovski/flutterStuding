a850a7f0fd85332a13355ed4a656db41243f2d57
Roman Gurniy <roman.gurniy@monogo.pl>
2021-08-22 23:27:56 +0300

    Chapter-2: counter

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,87 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    Center(
-      child: Text('Hello world', textDirection: TextDirection.ltr,)
-    )
-  );
+  runApp(Content());
+}
+
+class Content extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.indigo[200],
+          appBar: AppBar(
+            title: Text('Chapter2'),
+            centerTitle: true,
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Tap "-" to decrement',
+                  style: TextStyle(fontSize: 20),
+                ),
+                CounterWidget(),
+                Text(
+                  'Tap "+" to increment',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+}
+
+class CounterWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return CounterState();
+  }
+}
+
+class CounterState extends State<CounterWidget> {
+  int count = 50;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.grey,
+        border: Border.all(),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          IconButton(
+            onPressed: () {
+              setState(() {
+                count--;
+              });
+            },
+            icon: Icon(
+              Icons.remove,
+              size: 40,
+            ),
+          ),
+          Text(
+            '$count',
+            style: TextStyle(fontSize: 30),
+          ),
+          IconButton(
+            onPressed: () {
+              setState(() {
+                count++;
+              });
+            },
+            icon: Icon(Icons.add, size: 40),
+          ),
+        ],
+      ),
+    );
+  }
 }
