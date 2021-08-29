1bb511eec0e5182b3a49b63ff98fd583ad6b874f
Roman Gurniy <roman.gurniy@monogo.pl>
2021-08-29 15:48:33 +0300

    Finish weather view

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -51,8 +52,14 @@ Widget mainContent(){
                   searchBar(),
                   Divider(),
                   title(),
-                  Divider(),
+                  SizedBox(width: 0, height: 40),
                   weatherLogo(),
+                  SizedBox(width: 0, height: 40,),
+                  weatherInfo(),
+                  SizedBox(width: 0, height: 40,),
+                  appTitle(),
+                  Divider(),
+                  weatherList(),
                 ],
               ),
             )
@@ -75,7 +82,6 @@ Widget searchBar(){
     ],
   );
 }
-
 Widget title(){
   return Column(
     crossAxisAlignment: CrossAxisAlignment.center,
@@ -105,5 +111,140 @@ Widget weatherLogo(){
         ],
       )
     ],
+  );
+}
+Widget weatherInfo(){
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: <Widget> [
+      Column(
+        children: [
+          Icon(
+            Icons.wb_cloudy_sharp,
+            size: 30,
+            color: Colors.white,
+          ),
+          Text(
+            '5',
+            style: TextStyle(fontSize: 20, color: Colors.white),
+          ),
+          Text(
+            'km/h',
+            style: TextStyle(fontSize: 20, color: Colors.white),
+          ),
+        ],
+      ),
+      Column(
+        children: [
+          Icon(
+            Icons.wb_cloudy_sharp,
+            size: 30,
+            color: Colors.white,
+          ),
+          Text(
+            '3',
+            style: TextStyle(fontSize: 20, color: Colors.white),
+          ),
+          Text(
+            '%',
+            style: TextStyle(fontSize: 20, color: Colors.white),
+          ),
+        ],
+      ),
+      Column(
+        children: [
+          Icon(
+            Icons.wb_cloudy_sharp,
+            size: 30,
+            color: Colors.white,
+          ),
+          Text(
+            '20',
+            style: TextStyle(fontSize: 20, color: Colors.white),
+          ),
+          Text(
+            '%',
+            style: TextStyle(fontSize: 20, color: Colors.white),
+          ),
+        ],
+      ),
+    ],
+  );
+}
+Widget appTitle(){
+  return Text('7-DAY WEATHER FORECAST',
+    style: TextStyle(
+        fontSize: 20,
+        color: Colors.white,
+        fontWeight: FontWeight.w300
+    ),
+  );
+}
+
+class WeatherItem{
+  String day = '';
+  int temperature = 0;
+  String degree = '';
+  IconData icon = Icons.wb_sunny;
+  WeatherItem(String day, int temperature){
+    this.degree = '°C';
+    this.icon = Icons.wb_sunny;
+    this.day = day;
+    this.temperature = temperature;
+  }
+}
+Widget weatherList(){
+  List<WeatherItem> items = [];
+  items.add(WeatherItem('Friday', 20));
+  items.add(WeatherItem('Saturday', 15));
+  items.add(WeatherItem('Sunday', 18));
+  items.add(WeatherItem('Monday', 22));
+
+  return Container(
+    margin: const EdgeInsets.symmetric(vertical: 20.0),
+    height: 100,
+    child: ListView.separated(
+      itemCount: items.length,
+      physics: BouncingScrollPhysics(),
+      scrollDirection: Axis.horizontal,
+      shrinkWrap: true,
+      itemBuilder: (BuildContext context, int index){
+      return Container(
+                width: 150,
+                color: Colors.white.withOpacity(0.2),
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(width: 0,height: 10),
+                    Text('${items[index].day}',
+                    style: TextStyle(fontSize: 22,
+                        color: Colors.white,
+                    fontWeight: FontWeight.w400),
+                    ),
+                    SizedBox(width: 0,height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('${items[index].temperature} ',
+                          style: TextStyle(fontSize: 25,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w400),
+                        ),
+                        Text('${items[index].degree} ',
+                          style: TextStyle(fontSize: 25,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w400),
+                        ),
+                        Icon(items[index].icon,
+                        size: 30,
+                        color: Colors.white),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+    }, separatorBuilder: (BuildContext context, int index) {
+        return SizedBox(width: 15, height: 0,);
+    },
+    ),
   );
 }
